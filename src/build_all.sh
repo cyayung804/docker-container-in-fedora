8908682996898ec2b8a279ff3bf3748b280e80ee
cyayung804 <cyayung804@gmail.com>
2026-06-12 04:31:31 +0800

    Update build_all.sh

--- src/build_all.sh
+++ src/build_all.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-echo "==> Running $(dirname "$(realpath "$0")")/build_all.sh"
-
 date="$(date -Iseconds)"
+
 docker --version
 docker buildx version
 
@@ -18,11 +17,12 @@ function alpine()
 
     cd "src/${image_name}" || exit 1
 
-    while read -r IMAGE_TAG; do
+    while read -r IMAGE_TAG; do 
         if crane ls "${image_registry}/${image_name}" | grep -q "${IMAGE_TAG}"; then
             echo "${image_registry}/${image_name}:${IMAGE_TAG} already exists..."
             continue
         fi
+        
         export ALPINE_VERSION="${IMAGE_TAG}"
         export IMAGE_NAME="${image_name}"
         export IMAGE_REGISTRY="${image_registry}"
@@ -30,6 +30,7 @@ function alpine()
 
         echo "Building ${image_registry}/${image_name}:${IMAGE_TAG}..."
         docker buildx bake push --no-cache
+        
     done < <(echo "${alpine_versions}")
 }
 
@@ -52,7 +53,7 @@ function golang()
             if [[ "${IMAGE_TAG}" != "latest" ]] && crane ls "${image_registry}/${image_name}" | grep -q "${image_tag}"; then
                 echo "${image_registry}/${image_name}:${image_tag} already exists..."
             else
-
+            
                 export ALPINE_VERSION="${alpine_version}"
                 export GO_VERSION="${IMAGE_TAG}"
                 export IMAGE_NAME="${image_name}"
@@ -62,6 +63,7 @@ function golang()
                 echo "Building ${image_registry}/${image_name}:${image_tag}..."
                 docker buildx bake push --no-cache
             fi
+            
         done < <(echo "${go_versions}")
     done
 }
@@ -95,6 +97,7 @@ function terraform()
                 echo "Building ${image_registry}/${image_name}:${image_tag}..."
                 docker buildx bake push --no-cache
             fi
+            
         done < <(echo "${tf_versions}")
     done
 }
